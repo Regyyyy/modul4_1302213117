3d80a8b9af673586e6669dcb5dd3d2487b029f1c
PC7-IFLAB <120774163+Regyyyy@users.noreply.github.com>
2023-03-14 16:31:13 +0700

    Menambahkan class KodeBuah
    ditambahkan class KodeBuah

--- modul4_1302213117/modul4_1302213117/Program.cs
+++ modul4_1302213117/modul4_1302213117/Program.cs
@@ -10,6 +10,25 @@ namespace modul4_1302213117
     {
         static void Main(string[] args)
         {
+            //commit kedua
+            KodeBuah Buah = new KodeBuah();
+            Console.WriteLine("Buah Kurma");
+            Console.WriteLine("Kode Buah: " + Buah.getKodeBuah(KodeBuah.buah.Kurma));
+        }
+    }
+
+    public class KodeBuah
+    {
+        public enum buah { Apel, Aprikot, Alpukat, Pisang, Paprika, 
+            Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, 
+            Semangka }
+
+        public String getKodeBuah(buah Buah)
+        {
+            String[] kodeBuah = {"A00", "B00", "C00", "D00", "E00",
+            "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00",
+            "O00"};
+            return kodeBuah[(int)Buah];
         }
     }
 }
